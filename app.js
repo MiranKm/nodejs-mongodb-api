aa75be2df1a967dc88a919e916dc4a028a8286ec
MiranPC <miran9191@gmail.com>
2019-07-13 14:32:09 +0300

    sets up the port and body parser and adds first test get method

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ const express = require('express');
 
 const app = express()
 
-app.use(bodyParser.json())
-app.use(bodyparser.urlencoded({
-    extended: true
-}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.listen(8080,process.env.OPENSHIFT_NODEJS_IP || process.env.IP || '127.0.0.1', () => console.log('Express server is runnig at port no : 8080'));
+
+app.get('/hi',(request, response)=>{
+    response.send(`<p>hiiii</p>`)
+});
