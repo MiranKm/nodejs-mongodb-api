ec19f556a15bda125c068563d4fb884a6a3b9bf7
MiranPC <miran9191@gmail.com>
2019-07-13 17:11:08 +0300

    fixes the string parser deprecation on connect method to the db

--- app.js
+++ app.js
@@ -13,11 +13,13 @@ app.use(bodyParser.urlencoded({ extended: true , useNewUrlParser: true}));
 app.listen(8080,process.env.OPENSHIFT_NODEJS_IP || process.env.IP || '127.0.0.1', () => console.log('Express server is runnig at port no : 8080'));
 
 
-mongodbClient.connect(url, function(err, db) {
-    if (err) throw err;
-    console.log("Database created!");
+
+mongodbClient.connect(url,{ useNewUrlParser: true },(error,db )=>{
+    if (error) throw error;
+    console.log("connected");
+
     db.close();
-  });
+})
 
 
 app.get('/hi',(request, response)=>{
