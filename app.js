f021379ae560225b295703775a6132799dabb3dc
MiranPC <miran9191@gmail.com>
2019-07-24 20:10:33 +0300

    adds the layout files and the template designs for register, login, dashboard.

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const mongodbClient = require('mongodb').MongoClient ;
-const url = require('./url');
-
-
+const keys = require('./config/keys');
+const expressLayout= require("express-ejs-layouts")
+const mongoose = require('mongoose');
 const app = express()
+
+app.use(expressLayout);
+app.set('view engine', 'ejs');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true , useNewUrlParser: true}));
@@ -12,9 +15,11 @@ app.use(bodyParser.urlencoded({ extended: true , useNewUrlParser: true}));
 
 app.listen(8080,process.env.OPENSHIFT_NODEJS_IP || process.env.IP || '127.0.0.1', () => console.log('Express server is runnig at port no : 8080'));
 
+app.use('/', require('./routes/index'))
+app.use('/user', require('./routes/user'))
 
 
-mongodbClient.connect(url,{ useNewUrlParser: true },(error,db )=>{
+mongodbClient.connect(keys.MongoURL,{ useNewUrlParser: true },(error,db )=>{
     if (error) throw error;
     console.log("connected");
 
@@ -25,3 +30,5 @@ mongodbClient.connect(url,{ useNewUrlParser: true },(error,db )=>{
 app.get('/hi',(request, response)=>{
     response.send(`<p>hiiii</p>`)
 });
+
+
---
+++ routes/user.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+
+router.get("/login", (request, response)=> response.render("login"))
+router.get("/register", (request, response)=> response.render("register"))
+
+module.exports = router;
