a598d51fd571f63b0e6220fbe4721eb00bcf6a9c
MiranPC <miran9191@gmail.com>
2019-07-25 12:38:00 +0300

    fixes the user model checking if the object is empyty or not

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,48 @@
 const express = require('express');
 const router = express.Router();
 
-router.get("/login", (request, response)=> response.render("login"))
-router.get("/register", (request, response)=> response.render("register"))
+router.get("/login", (request, response) => response.render("login"))
+router.get("/register", (request, response) => response.render("register"))
+
+
+router.post('/register', (request, response) => {
+    const userObject = {
+        name: request.body.name,
+        email: request.body.email,
+        pass1: request.body.password,
+        pass2: request.body.password2,
+    }
+
+    const error = [];
+
+
+    if (!userObject.name || !userObject.email 
+        || !userObject.pass1 || !userObject.pass2) {
+        error.push({
+            msg: "Can't be empty"
+        })
+    }
+
+    if (userObject.pass1 !== userObject.pass2)
+        error.push({
+            msg: "Passwords Don't Match"
+        })
+
+    if (userObject.pass1 < 6) error.push({
+        msg: "Passwords Can't be less then 5 Charecters "
+    })
+
+    if (error > 0) {
+        response.render('register', {
+          error,
+          userObject,
+        });
+    } else {
+        response.send("Registerd")
+    }
+
+
+});
+
 
 module.exports = router;
